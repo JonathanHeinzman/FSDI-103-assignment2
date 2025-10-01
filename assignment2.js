5cbcc3e96af68e1b19102c3e39dfc74adef7b379
Jonathan Heinzman <jond.heinzman@icloud.com>
2025-10-01 11:02:31 -0700

    assignment 2

--- assignment2.js
+++ assignment2.js
@@ -3,5 +3,5 @@ function temperature(){
     let Celsius = Number(prompt("Enter the temperature in Celsius"));
     let Fahrenheit = (Celsius * 9/5) + 32; 
 
-    document.getElementById("results").innerHTML=`<p>${Fahrenheit}</p>`;
+    document.getElementById("results").innerHTML=`<p>The conversion of Celsius ${Celsius}&deg to Fahrenheit is ${Fahrenheit}&deg</p>`;
 }
